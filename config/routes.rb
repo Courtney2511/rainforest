1bdfb44cde5030420b0fc0e69f9d7168c5de3b25
Courtney Noonan <courtneynoonan@me.com>
2016-06-21 14:48:32 -0400

    Adding user profile page and link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show, :new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :products do
